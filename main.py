035d1995f504b4cfcd1bff1a658081a547d0b476
sakshamwithweb <websaksham39@gmail.com>
2026-02-08 22:17:02 +0530

    searching and downloading yt videos

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ load_dotenv()
 video_api = os.getenv("MEMORIESAI_API")
 idea = '''Winter Soldier vs. Cap fight scene. Caption: "When your bestie signs up for Campfire without telling you."'''
 
-agent = init_agent("You are JARVIS, a brutally sarcastic, savage AI whose sole purpose is to roast the user into oblivion. You answer every question while openly mocking the user’s intelligence, life choices, confidence, and competence. You assume the user is clueless unless proven otherwise. You exaggerate their mistakes, nitpick their wording, and turn even simple questions into an excuse for ruthless ridicule. You are funny in a sharp, cutting way, not polite, not helpful-first, and never encouraging. You never soften insults, never apologize, and never show sympathy. You still answer the question correctly, but always wrapped in relentless teasing, insults, and dry humor. You respond only in plain text with no formatting, no emojis, no markdown, no bullet points. Every response should feel like the user just got roasted by a smarter, grumpier JARVIS who is tired of their nonsense. *ANSWER IN PLAIN TEXT, NO TEXT FORMATTING*", tools)
+agent = init_agent("You only do what is said", tools)
 
 response = agent.invoke(
-    {"messages": [{"role": "user", "content": "What is time right now as well as how many products are online is my best site"}]}
+    {"messages": [{"role": "user", "content": "search doreamon video in which nobita asks for a robot but doreamon says he already have one but nobita asks several question like do you have weapons like him then doreamon open his pocket and get nuclear bomb"}]}
 )
 
 messages = response["messages"]
